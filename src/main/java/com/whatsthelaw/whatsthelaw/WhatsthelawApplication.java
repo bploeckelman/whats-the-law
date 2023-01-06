738c4733a520e93d58c2690939cf09df75ad1052
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2023-01-06 14:15:43 -0600

    Initial commit

---
+++ src/main/java/com/whatsthelaw/whatsthelaw/WhatsthelawApplication.java
@@ -0,0 +1,13 @@
+package com.whatsthelaw.whatsthelaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsthelawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsthelawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whatsthelaw/whatsthelaw/WhatsthelawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatsthelaw.whatsthelaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsthelawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
