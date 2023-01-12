0d7cf279a883ee949953d88178aea91dd47baa84
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2023-01-12 00:11:01 -0600

    Respond with 404 if requested doc isn't found

--- src/main/java/org/whatsthelaw/whatsthelaw/docs/DocController.java
+++ src/main/java/org/whatsthelaw/whatsthelaw/docs/DocController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/docs")
 @RequiredArgsConstructor
@@ -16,10 +14,9 @@ public class DocController {
     private final DocService docs;
 
     @GetMapping("/")
-    public String getDoc(@RequestParam int congress, @RequestParam int bill) throws IOException {
-        var version = 0;
-        var doc = docs.get(congress, bill, version);
-        return doc;
+    public String getDoc(@RequestParam int congress, @RequestParam int bill) {
+            var version = 0;
+            return docs.get(congress, bill, version);
     }
 
 }
--- src/main/java/org/whatsthelaw/whatsthelaw/docs/DocService.java
+++ src/main/java/org/whatsthelaw/whatsthelaw/docs/DocService.java
@@ -1,9 +1,10 @@
 package org.whatsthelaw.whatsthelaw.docs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,19 +14,28 @@ import java.nio.file.Paths;
 @Slf4j
 public class DocService {
 
-    public String get(int congress, int bill, int version) throws IOException, FileNotFoundException {
+    public static class DocNotFoundException extends ResponseStatusException {
+        public final String docName;
+        public DocNotFoundException(String docName) {
+            super(HttpStatus.NOT_FOUND, String.format("Document not found: '%s'", docName));
+            this.docName = docName;
+        }
+    }
+
+    public String get(int congress, int bill, int version) throws DocNotFoundException {
         var cwd = Paths.get("").toAbsolutePath().toString();
         log.debug("cwd: '{}'", cwd);
 
         var dir = Paths.get("docs");
-        var prefix = String.format("congress-%d-hr-%d-%d", congress, bill, version);
+        var docName = String.format("congress-%d-hr-%d-%d", congress, bill, version);
         try (var list = Files.list(dir)) {
             var optFilename = list
                     .filter(file -> !Files.isDirectory(file))
                     .map(Path::getFileName)
                     .map(Path::toString)
-                    .filter(file -> file.contains(prefix))
+                    .filter(file -> file.contains(docName))
                     .findFirst();
+
             if (optFilename.isPresent()) {
                 var filename = optFilename.get();
                 log.info("Found document: '{}'", filename);
@@ -33,10 +43,11 @@ public class DocService {
                 var body = Files.readString(path);
                 return body;
             } else {
-                var error = String.format("Document not found: '%s/%s*'", dir, prefix);
-                log.error(error);
-                throw new FileNotFoundException(error);
+                var fullDocName = String.format("%s/%s*", dir, docName);
+                throw new DocNotFoundException(fullDocName);
             }
+        } catch (IOException ex) {
+            throw new DocNotFoundException(docName);
         }
     }
 
