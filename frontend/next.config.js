48846f0ad4af4169e9c969bf3153de78c022e628
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2023-02-15 00:01:26 -0600

    Stub out basic member view

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.congress.gov',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
